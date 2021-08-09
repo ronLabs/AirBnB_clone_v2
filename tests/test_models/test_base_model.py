10ff85e71a95c686f6ca461dff52e614ce89b9ef
yoyogold-a11 <tmrfack@gmail.com>
2021-08-09 17:55:00 -0500

    adding tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -100,3 +100,51 @@ class TestinClassModel(unittest.TestCase):
         self.assertEqual(instance2.updated_at, instance2.updated_at)
         self.assertNotEqual(instance1.created_at, instance2.created_at)
         self.assertNotEqual(instance1.updated_at, instance2.updated_at)
+
+    def test_uuid(self):
+        """testing uuid"""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        for instance in [instance1, instance2]:
+            tuuid = instance.id
+            with self.subTest(uuid=tuuid):
+                self.assertIs(type(tuuid), str)
+
+        self.assertNotEqual(instance1.id, instance2.id)
+
+    def test_dictionary(self):
+        """testing to_dict correct funtionality"""
+        instance3 = BaseModel()
+        instance3.name = "Holbies"
+        instance3.my_number = 89
+        new_inst = instance3.to_dict()
+        expectec_attrs = ["id",
+                          "created_at",
+                          "updated_at",
+                          "name",
+                          "my_number",
+                          "__class__"]
+        self.assertCountEqual(new_inst.keys(), expectec_attrs)
+        self.assertEqual(new_inst['__class__'], 'BaseModel')
+        self.assertEqual(new_inst['name'], 'Holbies')
+        self.assertEqual(new_inst['my_number'], 89)
+
+    def test_str_method(self):
+        """testing str method, checking output"""
+        instance4 = BaseModel()
+        strr = "[BaseModel] ({}) {}".format(instance4.id, instance4.__dict__)
+        self.assertEqual(strr, str(instance4))
+
+    @mock.patch('models.storage')
+    def test_save_method(self, mock_storage):
+        """test save method and if it updates
+        "updated_at" calling storage.save"""
+        instance4 = BaseModel()
+        created_at = instance4.created_at
+        updated_at = instance4.updated_at
+        instance4.save()
+        new_created_at = instance4.created_at
+        new_updated_at = instance4.updated_at
+        self.assertNotEqual(updated_at, new_updated_at)
+        self.assertEqual(created_at, new_created_at)
+        self.assertTrue(mock_storage.save.called)
