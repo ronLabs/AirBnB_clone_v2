41ba4da86ff949dae885144bc68fe74f1eebcc45
yoyogold-a11 <tmrfack@gmail.com>
2021-08-09 16:10:10 -0500

    adding doc test

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""User test"""
+from datetime import datetime
+import inspect
+import models
+import pep8 as pycodestyle
+from models.base_model import BaseModel
+import time
+import unittest
+from unittest import mock
+Model = models.review.Review
+module_doc = models.review.__doc__
+path1 = "models/review.py"
+path2 = "tests/test_models/test_review.py"
+
+
+class DocsTest(unittest.TestCase):
+    """Test to check behaviors"""
+
+    @classmethod
+    def setUpClass(self):
+        """setting up tests"""
+        self.self_funcs = inspect.getmembers(Model, inspect.isfunction)
+
+    def test_pep8(self):
+        """Testing pep8"""
+        for path in [path1,
+                     path2]:
+            with self.subTest(path=path):
+                errors = pycodestyle.Checker(path).check_all()
+                self.assertEqual(errors, 0)
+
+    def test_module_docstring(self):
+        """Test module docstring"""
+        self.assertIsNot(module_doc, None,
+                         "review.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1,
+                        "test_review.py needs a docstring")
+
+        """Test classes doctring"""
+        self.assertIsNot(BaseModel.__doc__, None,
+                         "Review class needs a docstring")
+        self.assertTrue(len(BaseModel.__doc__) >= 1,
+                        "Review class needs a docstring")
+
+    def test_func_docstrings(self):
+        """test func dostrings"""
+        for func in self.self_funcs:
+            with self.subTest(function=func):
+                self.assertIsNot(
+                    func[1].__doc__,
+                    None,
+                    "{:s} method needs a docstring".format(func[0])
+                )
+                self.assertTrue(
+                    len(func[1].__doc__) > 1,
+                    "{:s} method needs a docstring".format(func[0])
+                )
