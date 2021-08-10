cd4ce17a7b45c98f1412b0748c55528ed2117e5f
yoyogold-a11 <tmrfack@gmail.com>
2021-08-10 15:33:55 -0500

    testin

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,6 +10,7 @@ import os
 import json
 import console
 import tests
+import inspect
 from console import HBNBCommand
 from models.base_model import BaseModel
 from models.user import User
@@ -19,7 +20,55 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
+Model = HBNBCommand
+City = HBNBCommand
+module_doc = HBNBCommand.__doc__
+path1 = "console.py"
+path2 = "tests/test_console.py"
 
+class TestBaseModelDocs(unittest.TestCase):
+    """Test to check behaviors"""
+
+    @classmethod
+    def setUpClass(self):
+        """setting up tests"""
+        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_pep8(self):
+        """Testing pep8"""
+        for path in [path1,
+                     path2]:
+            with self.subTest(path=path):
+                errors = pycodestyle.Checker(path).check_all()
+                self.assertEqual(errors, 0)
+
+    def test_module_docstring(self):
+        """Test module docstring"""
+        self.assertIsNot(module_doc, None,
+                         "base_model.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1,
+                        "base_model.py needs a docstring")
+
+    def test_class_docstring(self):
+        """Test classes doctring"""
+        self.assertIsNot(BaseModel.__doc__, None,
+                         "BaseModel class needs a docstring")
+        self.assertTrue(len(BaseModel.__doc__) >= 1,
+                        "BaseModel class needs a docstring")
+
+    def test_func_docstrings(self):
+        """test func dostrings"""
+        for func in self.base_funcs:
+            with self.subTest(function=func):
+                self.assertIsNot(
+                    func[1].__doc__,
+                    None,
+                    "{:s} method needs a docstring".format(func[0])
+                )
+                self.assertTrue(
+                    len(func[1].__doc__) > 1,
+                    "{:s} method needs a docstring".format(func[0])
+                )
 
 class ConsoleTest(unittest.TestCase):
     """testing console"""
@@ -55,6 +104,7 @@ class ConsoleTest(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("\n")
             self.assertEqual('', f.getvalue())
+
 
 class CreateTest(unittest.TestCase):
     """testing command test in console"""
--- tests/test_models/test_engine/test_dbstorage.py
+++ tests/test_models/test_engine/test_dbstorage.py
@@ -1,120 +1,31 @@
 #!/usr/bin/python3
-""" Module for testing file storage"""
 import unittest
-import models
-import inspect
-import time
-import json
-from unittest import mock
+import os.path
 import os
-from models.engine import db_storage
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.review import Review
-from models.state import State
-from models.place import Place
-from models.base_model import BaseModel
-from models import storage
-import pep8 as pycodestyle
+import models
+from datetime import datetime
+from models.engine.db_storage import DBStorage
+from models import *
+from console import HBNBCommand
 
-Model = db_storage.DBStorage
-FileStorage = db_storage.DBStorage
-path1 = "models/engine/db_storage.py"
-path2 = "tests/test_models/test_engine/test_dbstorage.py"
-module_doc = db_storage.DBStorage.__doc__
-
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "Review": Review,
-           "City": City, "User": User, "State": State, "Place": Place}
-
-
-class TestBaseModelDocs(unittest.TestCase):
-    """Test to check behaviors"""
-
-    @classmethod
+@unittest.skipIf(models.is_db != 'db', 'Not testing Dbstorage')
+class Test_dbstorage(unittest.TestCase):
+    """ testing dbstorage class """
     def setUpClass(self):
-        """setting up tests"""
-        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+        """setting up class"""
+        self.HNBNcom = HBNBCommand
+        self.Db = DBStorage
 
-    def test_pep8(self):
-        """Testing pep8"""
-        for path in [path1,
-                     path2]:
-            with self.subTest(path=path):
-                errors = pycodestyle.Checker(path).check_all()
-                self.assertEqual(errors, 0)
+        arg_list = {'updated_at': datetime(2021, 10, 8, 13, 31, 53, 331997),
+                     'id': 'c519fb40-3f5c-448b-945c-2ce8eaaf4931',
+                     'created_at': datetime(2021, 10, 8, 13, 31, 53, 331997),
+                     'name': 'holbie'}
+        self.model = City(arg_list)
 
-    def test_module_docstring(self):
-        """Test module docstring"""
-        self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
+    def test_init(self):
+        """testing init"""
+        self.assertNotEqual(self.Db._DBStorage__engine, None)
+        self.assertEqual(self.Db._DBStorage__session, None)
 
-    def test_class_docstring(self):
-        """Test classes doctring"""
-        self.assertIsNot(BaseModel.__doc__, None,
-                         "BaseModel class needs a docstring")
-        self.assertTrue(len(BaseModel.__doc__) >= 1,
-                        "BaseModel class needs a docstring")
-
-    def test_func_docstrings(self):
-        """test func dostrings"""
-        for func in self.base_funcs:
-            with self.subTest(function=func):
-                self.assertIsNot(
-                    func[1].__doc__,
-                    None,
-                    "{:s} method needs a docstring".format(func[0])
-                )
-                self.assertTrue(
-                    len(func[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(func[0])
-                )
-
-
-class Test_FileStorage(unittest.TestCase):
-    """Testing proper behavrious"""
-    def dic_tests(self):
-        """Testing that FileStorage_objects returns properly"""
-        f_storage = FileStorage()
-        dic = f_storage.all()
-        self.assertEqual(type(dic, dict))
-        self.assertIs(dic, f_storage.__Filestorage.__objects)
-
-    @unittest.skipIf(models.is_db != 'db', 'Not testing Dbstorage')
-    def test_new_method(self):
-        """testing that  new method saves on __objects attr"""
-        storage = FileStorage()
-        save = FileStorage._FileStorage__objects
-        FileStorage._FileStorage__objects = {}
-        test_dict = {}
-        for key, value in classes.items():
-            with self.subTest(key=key, value=value):
-                instance = value()
-                instance_key = instance.__class__.__name__ + "." + instance.id
-                storage.new(instance)
-                test_dict[instance_key] = instance
-                self.assertEqual(test_dict, storage._FileStorage__objects)
-        FileStorage._FileStorage__objects = save
-
-    @unittest.skipIf(models.is_db != 'db', 'Not testing Dbstorage')
-    def test_save(self):
-        """Testing proper behaviour saving in file.json"""
-        os.remove("file.json")
-        storage = FileStorage()
-        new_dict = {}
-        for key, value in classes.items():
-            instance = value()
-            instance_key = instance.__class__.__name__ + "." + instance.id
-            new_dict[instance_key] = instance
-        save = FileStorage._FileStorage__objects
-        FileStorage._FileStorage__objects = new_dict
-        storage.save()
-        FileStorage._FileStorage__objects = save
-        for key, value in new_dict.items():
-            new_dict[key] = value.to_dict()
-        string = json.dumps(new_dict)
-        with open("file.json", "r") as f:
-            js = f.read()
-        self.assertEqual(json.loads(string), json.loads(js))
+    def test_all_method(slef):
+        """testing that all method return all args"""
