1c208d2654650f54c61ce036497aa9313a17e7ec
rennleon <2829@holbertonschool.com>
2021-08-26 23:39:24 +0000

    Replace imports with actual code in order to run the script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,11 +4,52 @@ deploys static content to web servers"""
 from fabric.api import env, put, run
 from datetime import datetime as dt
 from os import path
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['35.243.194.38', '54.91.79.148']
 path_releases = '/data/web_static/releases'
+
+
+def do_pack():
+    """This function packs the contents of web_static into a tgz archive"""
+    td = dt.utcnow()
+    tgzname = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+        .format(td.year, td.month, td.day, td.hour, td.minute, td.second)
+
+    if local('mkdir -p versions').failed:
+        return None
+    if local('tar -czvf {} web_static'.format(tgzname)).failed:
+        return None
+    return tgzname
+
+
+def do_deploy(archive_path):
+    """This function distributs an archive file"""
+    if not path.isfile(archive_path):
+        return False
+    filename = archive_path.split('/')[-1]
+    fname = filename.split('.')[0]
+    if put(archive_path, '/tmp/{}'.format(filename)).failed:
+        return False
+    if run('mkdir -p /data/web_static/releases/{}'
+            .format(fname)).failed:
+        return False
+    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(filename, fname)).failed:
+        return False
+    if run('rm /tmp/{}'.format(filename)).failed:
+        return False
+    path_web = '{}/{}'.format(path_releases, fname)
+    if run('mv {}/web_static/* {}'
+            .format(path_web, path_web)).failed:
+        return False
+    if run('rm -rf {}/web_static'.format(path_web)).failed:
+        return False
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+    if run('ln -s {} /data/web_static/current'.
+            format(path_web)).failed:
+        return False
+    return True
 
 
 def deploy():
