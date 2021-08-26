523a37a880cabbf4b5b4b378537ca0e43c326933
rennleon <2829@holbertonschool.com>
2021-08-26 13:29:13 +0000

    Create a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a fabric script that
+deploys static content to web servers"""
+from fabric.api import env, put, run
+from datetime import datetime as dt
+from os import path
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env. hosts = ['35.243.194.38', '54.91.79.148']
+path_releases = '/data/web_static/releases'
+
+
+def deploy():
+    """This function deploys the content to a server"""
+    path = do_pack()
+    if not path:
+        return False
+    return do_deploy(path)
