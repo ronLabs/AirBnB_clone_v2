5666c0857c23f05fa9b06974448f633a3342c5d6
ronLabs <ron.altamir@outlook.com>
2022-04-29 05:01:12 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,18 +13,26 @@ def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
-    try:
-        file_ext = archive_path.split("/")[-1]
-        file_no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_ext, path, file_no_ext))
-        run('rm /tmp/{}'.format(file_ext))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
-        run('rm -rf {}{}/web_static'.format(path, file_no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_ext))
-        return True
-    except:
+
+    file_ext = archive_path.split("/")[-1]
+    file_no_ext = file_n.split(".")[0]
+    path = "/data/web_static/releases/"
+    if put(archive_path, '/tmp/{}'.format(file_ext)).failed:
         return False
+    if run('mkdir -p {}{}/'.format(path, file_no_ext)).failed:
+        return False
+    if run('tar -xzf /tmp/{} -C {}{}/'
+            .format(file_ext, path, file_no_ext)).failed:
+        return False
+    if run('rm /tmp/{}'.format(file_ext)).failed:
+        return False
+    if run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext)).failed:
+        return False
+    if run('rm -rf {}{}/web_static'.format(path, file_no_ext)).failed:
+        return False
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+    if run('ln -s {}{}/ /data/web_static/current'
+           .format(path, file_no_ext)).failed:
+        return False
+    return True
