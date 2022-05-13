d104d47343eab2300b3bbb578bb37c2d8d78a0bd
ronLabs <ron.altamir@outlook.com>
2022-05-13 22:46:37 +0000

    script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module starts a flask app on 0.0.0.0:5000"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """Returns a text"""
+    return 'Hello HBNB!'
+
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display(text):
+    """Returns text passed as a parameter"""
+    return 'c {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
