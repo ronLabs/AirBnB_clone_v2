42baddd7af89a957e9242c37bb0ce95b41fc5562
ronLabs <ron.altamir@outlook.com>
2022-05-16 15:45:33 +0000

    script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module starts a flask app on 0.0.0.0:5000"""
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,7 +8,6 @@ app = Flask(__name__)
 def main():
     """Returns a text"""
     return 'Hello HBNB!'
-
 
 
 @app.route('/hbnb', strict_slashes=False)
